e3a26e1f3fc6fb2308201b8ba9e52d9e343e73d3
Samuel Oliveira Lopes <samu5020ol@gmail.com>
2024-01-15 09:48:21 -0300

    peca1

--- main.cpp
+++ main.cpp
@@ -10,38 +10,46 @@ int main () {
     const int screenWidth = 300;
     const int screenHeight = 600;
 
-    int linhas;
-    int colunas;
+    //int linhas;
+    //int colunas;
     int grade[20][10];
     int tamanhoCedula = 30;
-    int valorCedula;
+    //int valorCedula;
 
-
-    for(int linha = 0; linha < 20; linha++){
-                for(int coluna = 0; coluna < 10; coluna++){
-                    grade[linha][coluna] = coluna;
-                }
-            }
-
-            for(int linhas = 0; linhas < 20; linhas++){
-                for(int colunas = 0; colunas < 10; colunas++){
-                    std::cout << grade[linhas][colunas] << " ";
-                }
-                std::cout << endl;
-            }
-
-    cout << "Hello World" << endl;
+    int jpeca[3][3];
 
     InitWindow(screenWidth, screenHeight, "My first TETRIS program!");
     SetTargetFPS(60);
 
-    while (!(WindowShouldClose())){ 
 
-        for(int i=0;i<20;i++){
-            for(int j=0;j<10;j++){
-                grade[i][j] = GetRandomValue(0,6);
-            }
+    for(int linha = 0; linha < 20; linha++){
+                for(int coluna = 0; coluna < 10; coluna++){
+                    grade[linha][coluna] = 0;
+                }
         }
+
+        
+
+            //for(int linhas = 0; linhas < 20; linhas++){
+            //    for(int colunas = 0; colunas < 10; colunas++){
+            //        std::cout << grade[linhas][colunas] << " ";
+            //    }
+            //    std::cout << endl;
+            //}
+
+    //cout << "Hello World" << endl;
+
+
+
+    for(int i=0;i<20;i++){
+            for(int j=0;j<10;j++){
+                grade[i][j] = GetRandomValue(0,7);
+                cout << grade[i][j] << " ";
+            }
+            cout << endl;
+        }
+
+    while (!(WindowShouldClose())){ 
 
         BeginDrawing();
 
@@ -61,14 +69,12 @@ int main () {
                         DrawRectangle(colunas * tamanhoCedula - 1, linhas * tamanhoCedula - 1, tamanhoCedula - 1, tamanhoCedula - 1, ORANGE);
                     }else if(grade[linhas][colunas] == 6){
                         DrawRectangle(colunas * tamanhoCedula - 1, linhas * tamanhoCedula - 1, tamanhoCedula - 1, tamanhoCedula - 1, GREEN);
+                    } else if(grade[linhas][colunas] == 7){
+                        DrawRectangle(colunas * tamanhoCedula - 1, linhas * tamanhoCedula - 1, tamanhoCedula - 1, tamanhoCedula - 1, DARKBLUE);
                     }
                 }
             }
         
-        
-            
-            
-
             ClearBackground(RAYWHITE);
 
             //DrawText("Congrats! You created \n your first window!", 100, 200, 10, LIGHTGRAY);
